ae829b3228126ade20ed62c7fdbd197c54d427ee
saurabh rajpoot <saurabh941193@gmail.com>
2023-10-07 12:47:15 +0530

    extra file deleted

--- src/Components/Add New Property/Basicinfo.js
+++
@@ -1,4 +0,0 @@
-import {useContext} from 'react';
-import Button from "../Add New Property/Buttons.js";
-import PropertyContext from "../Add New Property/Contextprovider.js"
-import Navbar from "../Add New Property/Pagenavbar.js"
--- src/Components/Login/Login.js
+++
@@ -1,4 +0,0 @@
-import React, { useState } from "react";
-import logo from  "../Images/Realestatelogo.jpg"
-import axios from "axios";
-import '../Styles/Login.css'
