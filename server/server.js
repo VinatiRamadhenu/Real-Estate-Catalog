8be16748ca6aea64b2e66bc90fa0b96b3d9ded1b
saurabh rajpoot <saurabh941193@gmail.com>
2023-10-07 15:52:24 +0530

    changend origin for axios

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://therealestate.onrender.com",
 };
 
 app.use(cors(corsOptions));
